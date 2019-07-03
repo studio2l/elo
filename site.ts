0bf09be64c3f96d8740713742f443bbb365969fe
kim yongbin <kybinz@gmail.com>
2019-07-03 14:22:38 +0900

    site: SITE_ROOT 미설정 알림에 경로명 / 로 표시

--- site.ts
+++ site.ts
@@ -27,7 +27,7 @@ export function Init() {
     showRoot = path.normalize(showRoot)
     let siteFile = path.join(siteRoot, "site.json")
     if (!fs.existsSync(siteFile)) {
-        throw Error("$SITE_ROOT에site.json 파일이 없습니다. example/site.json 파일을 복사해서 사용하셔도 됩니다.")
+        throw Error("$SITE_ROOT/site.json 파일이 없습니다. example/site.json 파일을 복사해서 사용하셔도 됩니다.")
     }
     let data = fs.readFileSync(siteFile)
     siteInfo = JSON.parse(data.toString("utf8"))
